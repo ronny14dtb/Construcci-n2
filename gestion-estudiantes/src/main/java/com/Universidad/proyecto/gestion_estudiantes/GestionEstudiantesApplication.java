eff6806235ce463b1c4abefb350635ef042291d8
ronny14dtb <ronnydario8@outlook.com>
2025-08-24 02:29:34 -0500

    Primer commit con proyecto Spring Boot

---
+++ gestion-estudiantes/src/main/java/com/Universidad/proyecto/gestion_estudiantes/GestionEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.Universidad.proyecto.gestion_estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEstudiantesApplication.class, args);
+	}
+
+}
